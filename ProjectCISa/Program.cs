7a9b539a166e849146188599651fd8b29e9f770b
Sirichaiz <39221774+Sirichaiz@users.noreply.github.com>
2023-06-01 16:25:25 +0700

    Update Program.cs

--- ProjectCISa/Program.cs
+++ ProjectCISa/Program.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+กหหหหดดดหหห
 namespace ProjectCIS
 {
     static class Program
